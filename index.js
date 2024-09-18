213963042bafb1a39ede5099cbcfec0d7d9a52af
Ethan <ethanyouso@gmail.com>
2024-09-18 17:23:26 -0500

    Add feature carousels

--- index.js
+++ index.js
@@ -15,10 +15,22 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   const carouselImages = fs.readdirSync(path.join(__dirname, "public/assets/carousel"))
     .filter(file => file.endsWith(".jpg") || file.endsWith(".png")); // Filter for image files
+    const carousel1Images = fs.readdirSync(path.join(__dirname, "public/assets/feature/featureCarousel1"))
+    .filter(file => file.endsWith(".jpg") || file.endsWith(".png"));
+  const carousel2Images = fs.readdirSync(path.join(__dirname, "public/assets/feature/featureCarousel2"))
+    .filter(file => file.endsWith(".jpg") || file.endsWith(".png"));
+  const carousel3Images = fs.readdirSync(path.join(__dirname, "public/assets/feature/featureCarousel3"))
+    .filter(file => file.endsWith(".jpg") || file.endsWith(".png"));
+  const carousel4Images = fs.readdirSync(path.join(__dirname, "public/assets/feature/featureCarousel4"))
+    .filter(file => file.endsWith(".jpg") || file.endsWith(".png"));
 
   res.render("index.ejs", { 
     year: new Date().getFullYear(),
-    carouselImages 
+    carouselImages,
+    carousel1Images,
+    carousel2Images,
+    carousel3Images,
+    carousel4Images
   });
 });
 
